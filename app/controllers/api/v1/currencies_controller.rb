a022eb6fada09b2db52d16a8730484d89db93e8a
Alex Smirnov <sas2job@gmail.com>
2022-03-06 10:02:29 +0300

    Add links for pagination

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -1,11 +1,11 @@
 module Api
   module V1
     class CurrenciesController < ApplicationController
-
       # CET /currencies
       def index
-        currencies = Currency.page(page).per(per_page)
-        render json: CurrencySerializer.render_as_hash(currencies, view: :normal)
+        @currencies = Currency.page(page).per(per_page)
+        render json: CurrencySerializer.render_as_hash(@currencies, view: :normal)
+        set_pagination_headers(@currencies)
       end
 
       private
@@ -18,6 +18,21 @@ module Api
         per_page ||= params[:per_page] || 10
       end
 
+      def set_pagination_headers(cur_col)
+        headers["X-Total-Count"] = cur_col.total_count
+
+        links = []
+        links << page_link(1, "first") unless cur_col.first_page?
+        links << page_link(cur_col.prev_page, "prev") if cur_col.prev_page
+        links << page_link(cur_col.next_page, "next") if cur_col.next_page
+        links << page_link(cur_col.total_pages, "last") unless cur_col.last_page?
+        headers["Link"] = links.join(", ") if links.present?
+      end
+
+      def page_link(page, rel)
+        base_url = request.url.split("?").first
+        "<#{base_url}?#{request.query_parameters.merge(page: page).to_param}>; rel='#{rel}'"
+      end
     end
   end
 end
